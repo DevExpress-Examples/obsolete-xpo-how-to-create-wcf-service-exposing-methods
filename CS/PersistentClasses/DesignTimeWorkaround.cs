52104918eb53237c2d9ccae90c377987169bfdc8
lexkazakov <alexey.kazakov@devexpress.com>
2021-09-10 15:41:43 +0300

    obsolete this example

--- CS/PersistentClasses/DesignTimeWorkaround.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DXSample.PersistentClasses {
-    /// <summary>
-    /// This class is necessary for design-time support, and can be removed from the release version.
-    /// See the http://www.devexpress.com/Support/Center/KB/ViewKBIssue.aspx?kbid=K18450 Knowledge Base article for details.
-    /// </summary>
-    public static class DesignTimeWorkaround {
-        public static void Register() { }
-    }
-}
